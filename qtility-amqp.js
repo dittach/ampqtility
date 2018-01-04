720187dd0bdca23a125e32cb335ad4a45548cd70
Git-Admin <morfie78@gmail.com>
2018-01-04 00:39:00 -0500

    removed old unused code

--- qtility-amqp.js
+++ qtility-amqp.js
@@ -74,7 +74,7 @@ switch (program.op) {
         handleTestAsync();
     break;
     case 'persist':
-        handlePersist2();
+        handlePersist();
     break;
     case 'movequeues':
         handleMoveQueues();
@@ -178,7 +178,7 @@ function cancelWhenEmpty(queueName, consumerTag) {
 }
 
 
-function handlePersist2() {
+function handlePersist() {
     // connect to amqp
     amqp(app, amqpSettings).connect(async function() {
         try {
@@ -211,35 +211,6 @@ function handlePersist2() {
     });
 }
 
-
-if (program.op === "test") {
-   // handleTestAsync();
-} else if (1>2) {
-
-    amqp(app, amqpSettings).connect(function () {
-        var debugThisFunction = true;
-        var fName = 'amqp.copnnect():';
-
-        if (process.send) process.send('online');
-        //do stuff
-
-        createTempQueue(function () {
-            if (enableDebugMsgs && debugThisFunction) { console.log(modName, fName, 'createTempQueue callback. op:', program.op); }
-            if (program.op === "persist") {
-                handlePersist();
-            } else if (program.op === "movequeues") {
-                //sourcequeue
-                //destqueue
-            }/* else if (program.op === "test") {
-            handleTest();
-        }*/
-
-        });
-
-        app.ready = true;
-    });
-}
-
 async function handleTestAsync() {
     const debugThisFunction = true;
     const fName = 'handleTestAsync():';
@@ -258,103 +229,6 @@ async function handleTestAsync() {
     await channel.publish(program.sourcequeue, exchangeBindings, new Buffer(JSON.stringify(content)) );
     await channel.close();
     await conn.close();
-}
-
-
-function handleTest() {
-    var debugThisFunction = true;
-    var fName = 'handleTest():';
-    if (enableDebugMsgs && debugThisFunction) { console.log(modName, fName, 'called.'); }
-
-    console.log('Adding 3 test messages to queue', program.sourcequeue);
-    var messageOptions = {
-        mandatory: true,
-        contentType: 'application/json',
-        deliveryMode: 2, // persistent
-        persistent: true
-    };
-    var content = {"test1":"test1"};
-    console.log('1:', app.amqp.publish(program.sourcequeue, exchangeBindings, new Buffer(JSON.stringify(content)), messageOptions));
-    content = {"test2":"test2"};
-    console.log('2:', app.amqp.publish(program.sourcequeue, exchangeBindings, new Buffer(JSON.stringify(content)), messageOptions));
-    content = {"test3":"test3"};
-    console.log('3:', app.amqp.publish(program.sourcequeue, exchangeBindings, new Buffer(JSON.stringify(content)), messageOptions));
-    console.log('done');
-    setTimeout(function() { cleanupAndShutdown(); }), 5000;
-}
-
-function handlePersist() {
-    var debugThisFunction = true;
-    var fName = 'handlePersist():';
-
-    if (enableDebugMsgs && debugThisFunction) { console.log(modName, fName, 'called.'); }
-
-    app.amqpHelpers.subscribeDirect(program.sourcequeue, function (error, content, message, messageCount) {
-        if (error) return app.amqp.reject(message);
-
-        if (queuePoll === undefined) {
-            queuePoll = setInterval(endWhenEmpty, 5000);
-        }
-
-        if (!content) {
-            console.log("malformed message received on", program.sourcequeue, content, message);
-            return app.amqp.reject(message);
-        }
-
-        console.log("messageCount:", messageCount);
-        console.log("received new", program.sourcequeue);
-
-        var messageOptions = {
-            mandatory: true,
-            contentType: 'application/json',
-            deliveryMode: 2, // persistent
-            persistent: true
-        };
-
-        if (message.properties !== null) {
-            _.extend(messageOptions, message.properties);
-        }
-        _.extend(messageOptions, {
-            deliveryMode: 2,
-            persistent: true
-        });
-
-        if (messageCount > 0) {
-            try {
-                app.amqp.publish(tempqueue, exchangeBindings, new Buffer(JSON.stringify(content)), messageOptions);
-                app.amqp.ack(message, false);
-            } catch (e) {
-                console.log("qtility", "Unable to publish on", tempqueue, exchangeBindings, "with message", JSON.stringify(message), "error:", e.message);
-                app.amqp.reject(message);
-            }
-        } else {
-            console.log('No messages found. Exiting.');
-            cleanupAndShutdown();
-        }
-    });
-}
-
-function createTempQueue(callback) {
-    var debugThisFunction = true;
-    var fName = 'createTempQueue():';
-
-    if (enableDebugMsgs && debugThisFunction) { console.log(modName, fName, 'called.'); }
-    app.amqp.assertExchange(tempqueue, exchangeOptions.type).then(function (ex) {
-        app.amqp.assertQueue(tempqueue, {
-            durable: true,
-            autoDelete: false,
-            confirm: true
-        }).then(function (q) {
-            app.amqp.bindQueue(tempqueue, tempqueue, exchangeBindings).then(function () {
-                console.log('qtility', 'queue', tempqueue, 'bound successfully on exchange', tempqueue, exchangeBindings);
-            }).catch(function (err) {
-                console.log('qtility', 'error during queue setup', err);
-                callback(err);
-            });
-        });
-    });
-
-    callback(null);
 }
 
 function getQueueCount(queueName, emptyCallback) {
@@ -384,115 +258,6 @@ function getQueueCount(queueName, emptyCallback) {
             emptyCallback(results.messageCount);
         });
     }
-}
-
-function endWhenEmpty() {
-    var debugThisFunction = true;
-    var fName = 'endWhenEmpty():';
-
-    if (enableDebugMsgs && debugThisFunction) { console.log(modName, fName, 'called.'); }
-    getQueueCount(program.sourcequeue, function (messageCount) {
-        console.log('endWhenEmpty(), messageCount:', messageCount);
-
-        if (messageCount === 0) {
-            if (queuePoll !== undefined) {
-                clearInterval(queuePoll);
-            }
-
-            // unsubscribe from the source queue
-            unsubscribeFromQueueAndExchange(program.sourcequeue, moveItemsBack);
-            //cleanupAndShutdown();
-        }
-    });
-}
-
-function unsubscribeFromQueueAndExchange(queueName, callback) {
-    var debugThisFunction = true;
-    var fName = 'unsubscribeFromQueueAndExchange():';
-
-    if (enableDebugMsgs && debugThisFunction) { console.log(modName, fName, 'called.'); }
-    app.amqp.cancel('qtility').then(function () {
-        callback();
-    });
-}
-
-function moveItemsBack(err) {
-    var debugThisFunction = true;
-    var fName = 'moveItemsBack():';
-
-    if (enableDebugMsgs && debugThisFunction) { console.log(modName, fName, 'called.'); }
-    // subscribe to tempqueue as a consumer
-    if (err) {
-        console.log('Unable to unsubscribe from source queue', program.sourcequeue);
-        cleanupAndShutdown();
-    } else {
-        subscribeToTempQueue(function () {
-            if (enableDebugMsgs && debugThisFunction) { console.log(modName, fName, 'subscribeToTempQueue() callback.'); }
-        });
-    }
-
-}
-
-
-function subscribeToTempQueue(callback) {
-    var debugThisFunction = true;
-    var fName = 'subscribeToTempQueue():';
-
-    if (enableDebugMsgs && debugThisFunction) { console.log(modName, fName, 'called.'); }
-
-
-    app.amqpHelpers.subscribeDirect(tempqueue, function (error, content, message, messageCount) {
-
-        if (enableDebugMsgs && debugThisFunction) { console.log(modName, fName, 'subscribeDirect() callback.'); }
-
-        if (error) return app.amqp.reject(message);
-
-        if (queueEndPoll === undefined) {
-            if (enableDebugMsgs && debugThisFunction) { console.log(modName, fName, 'subscribeDirect() callback: initializing queueEndPoll setInterval'); }
-            queueEndPoll = setInterval(endMoveBackWhenEmpty, 5000);
-        }
-
-        if (!content) {
-            console.log("malformed message received on", tempqueue, content, message);
-            return app.amqp.reject(message);
-        }
-
-        console.log("messageCount:", messageCount);
-        console.log("received new", tempqueue);
-
-        if (messageCount > 0) {
-            if (enableDebugMsgs && debugThisFunction) { console.log(modName, fName, 'subscribeDirect(): messageCount', messageCount); }
-            try {
-                app.amqp.publish(program.sourcequeue, exchangeBindings, new Buffer(JSON.stringify(content)), message.properties);
-                app.amqp.ack(message, false);
-                if (enableDebugMsgs && debugThisFunction) { console.log(modName, fName, 'subscribeDirect(): published to', program.sourcequeue,'and ackd.'); }
-            } catch (e) {
-                console.log("qtility", "Unable to publish on", program.sourcequeue, exchangeBindings, "with message", JSON.stringify(message), "error:", e.message);
-                app.amqp.reject(message);
-            }
-        }
-
-    });
-
-    callback(null);
-}
-
-
-function endMoveBackWhenEmpty() {
-    var debugThisFunction = true;
-    var fName = 'endMoveBackWhenEmpty():';
-
-    if (enableDebugMsgs && debugThisFunction) { console.log(modName, fName, 'called.'); }
-    getQueueCount(tempqueue, function (messageCount) {
-        console.log('endMoveBackWhenEmpty(), messageCount:', messageCount);
-
-        if (messageCount === 0) {
-            if (queueEndPoll !== undefined) {
-                clearInterval(queueEndPoll);
-            }
-            cleanupAndShutdown();
-        }
-    });
 }
 
 

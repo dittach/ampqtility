692db0047bb7be019945d3e40787379dcbeb8f59
Git-Admin <morfie78@gmail.com>
2018-01-07 12:49:44 -0500

    added counter to output

--- qtility-amqp.js
+++ qtility-amqp.js
@@ -160,7 +160,7 @@ async function moveQueues(srcqueue, destqueue, options) {
             }
 
             if (itemctr%10000===9999) {
-                console.log('Processed 10000 records, sleeping.');
+                console.log('Processed 10000 records, sleeping. Currently at', itemctr);
                 try { await sleep(5000); } catch(err) { console.log(err); }
             }
           }, {
